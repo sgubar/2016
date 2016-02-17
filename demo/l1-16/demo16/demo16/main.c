5989c6a9964d8a4a12b74d45d75a176d9915d6eb
Slava Gubar <sl@Slavas-MBP.dark>
2016-02-17 22:55:30 +0200

    Demo for l1/slide 16

---
+++ demo/l1-16/demo16/demo16/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  demo16
+//
+//  Created by Slava Gubar on 2/14/16.
+//  Copyright © 2016 Slava Gubar. All rights reserved.
+//
+
+#include <stdio.h>
+#include "SumComplexity.h"
+#include "MulComplexity.h"
+
+int main(int argc, const char * argv[])
+{
+	doSumComplexity();
+	doMulComplexity();
+	
+	printf("Thanks!");
+	
+   return 0;
+}
