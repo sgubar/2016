8fcb332096ea8bd4d6c3ba7e7a51a27c0216e1eb
Aleksander <cropemail@gmail.com>
2016-02-26 22:01:48 +0200

    That version is final, for now.

--- dk51/AMahnyov/lab1/calculate.c
+++ dk51/AMahnyov/lab1/calculate.c
@@ -7,17 +7,12 @@ static void doFormula(int arguments[2]);
 
 void doCalculation()
 {
-    char *answer = "y";
-    while(answer == "y" || answer == "Y"){
-    int *arguments;
+    int *arguments; //pointer for an array
     arguments = getInput(2); //gets input from user, as an array: A-0 B-1 C-2
     doFormula(arguments);
-    printf("Again? [Y/N]: ");
-    scanf("%с", answer);
-    }
 }
 
 void doFormula(int arguments[2]){ //calculates the result, with bicyleMath formulas
     double finalResult = bicycleModulo(arguments[0]*arguments[2]-arguments[1]) / bicyclePow(arguments[2], 3) * bicycleSum(arguments[0]); //main formula
-    printf("Final result: %f", finalResult);
+    printf("Final result: %f\n", finalResult);
 }
--- dk51/AMahnyov/lab1/getInput.c
+++ dk51/AMahnyov/lab1/getInput.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 int getInput(){
-    int i;
     int arguments[2];
     printf("Enter the input values, with space separation: \n");
     scanf("%d %d %d", &arguments[0], &arguments[1], &arguments[2]);
