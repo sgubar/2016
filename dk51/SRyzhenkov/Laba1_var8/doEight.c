92faeb895b820c2b7e864d7cbbfcbd79cccced86
Serhii <Sergiy905@gmail.com>
2016-03-09 01:11:45 +0200

    Сhanges for first lab

--- dk51/SRyzhenkov/Laba1_var8/doEight.c
+++ dk51/SRyzhenkov/Laba1_var8/doEight.c
@@ -1,20 +1,27 @@
+//
+// Laba. 1 var. 8
+//
+//  Created by Ryzhenkov Serhii on 08/03/16.
+//  Copyright © 2016 Ryzhenkov Serhii. All rights reserved.
 
  int doEight();
  int doEight()
  {
     int result;
     int iter = 0;
-    int output[100];
+    int output[100];    //Создали и объявили массив для записи остачи от деления
     int i;
-    printf("Enter the number: ");
+    printf("Enter the number: "); //запросили данные у пользователя
     scanf("%d", &result);
-    while(result>=8){
-        output[iter] = result%8;
+    //цикл для выполнения поставленной нам задачи
+    while(result>=8){                   //Пока число, которое ввел пользователь, делится на 8, цикл выолняется
+        output[iter] = result%8;        //запись остачи от деления в массив
         result /= 8;
         iter++;
     }
-    output[iter] = result;
-    for(i = iter; i>=0; i--){
+    output[iter] = result;              //записываем в массив последнее число, при котором мы вышли из цикла
+    for(i = iter; i>=0; i--)            //цикл для записи данных массива в обратной последовательности
+    {
         printf("%d", output[i]);
     }
  }
--- dk51/SRyzhenkov/Laba1_var8/doEight.h
+++ dk51/SRyzhenkov/Laba1_var8/doEight.h
@@ -1,3 +1,9 @@
+//
+// Laba. 1 var. 8
+//
+//  Created by Ryzhenkov Serhii on 08/03/16.
+//  Copyright © 2016 Ryzhenkov Serhii. All rights reserved.
+
 #ifndef DOEIGHT_H_INCLUDED
 #define DOEIGHT_H_INCLUDED
 
