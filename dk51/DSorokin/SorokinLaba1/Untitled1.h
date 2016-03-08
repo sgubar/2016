02ca17951a0d4e99779a4ea639ecb79c3d0e934c
dsorokin15 <dimcha-98@ukr.net>
2016-03-08 17:38:03 +0200

    First try

---
+++ dk51/DSorokin/SorokinLaba1/Untitled1.h
@@ -0,0 +1,14 @@
+//
+//
+//  Laboratorna1
+//
+//  Created by Sorokin Dmytro on 07/03/16.
+//  Copyright © 2016 Sorokin Dmytro. All rights reserved.
+//
+
+#ifndef UNTITLED1_H_INCLUDED
+#define UNTITLED1_H_INCLUDED
+
+int doFactorial (int B);
+int doPower(int C);
+#endif // UNTITLED1_H_INCLUDED
