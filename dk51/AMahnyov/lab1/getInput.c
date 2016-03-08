871a1fa43c99a255e3d3c1eee724b3e136f25108
Aleksander <cropemail@gmail.com>
2016-03-08 20:05:44 +0200

    Even more cleaning up

--- dk51/AMahnyov/lab1/getInput.c
+++
@@ -1,9 +0,0 @@
-#include "getInput.h"
-#include <stdio.h>
-
-int getInput(){
-    int arguments[2];
-    printf("Enter the input values, with space separation: \n");
-    scanf("%d %d %d", &arguments[0], &arguments[1], &arguments[2]);
-    return(arguments);
-}
