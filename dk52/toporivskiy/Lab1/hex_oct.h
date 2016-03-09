4feab2faa44d6a782897aace7064ae67a0259898
toporivskiy <toporivskie@gmail.com>
2016-03-10 00:48:55 +0200

    Added files via upload

---
+++ dk52/toporivskiy/Lab1/hex_oct.h
@@ -0,0 +1,16 @@
+//
+//  hex_oct.h
+//  Lab1
+//
+//  Created by Andrew Toporivskiy on 09/03/16 (DD/MM/YY).
+//  Copyright (C) 2016 Andrew Toporivskiy. All rights reserved.
+//
+
+
+#pragma once
+#ifndef HEX_OCT
+#define HEX_OCT
+
+void hex_oct();
+
+#endif /* HEX_OCT */
