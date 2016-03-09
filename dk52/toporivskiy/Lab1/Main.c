cd5327a57a39a3641f901a0816e6c37d3cf48e1e
toporivskiy <toporivskie@gmail.com>
2016-03-10 00:05:05 +0200

    Temporary version
    Don't work hex_oct_Number. Can not scan stream to array =(

--- dk52/toporivskiy/Lab1/Main.c
+++ dk52/toporivskiy/Lab1/Main.c
@@ -10,15 +10,38 @@
 #include <stdio.h>
 
 #include "dec_bin.h"
-//#include "hex_oct.h"
+#include "hex_oct.h"
 
 int main()
 {
-	int number;
-	printf("Enter decimal numer (no more than 1048576): \n");
-	scanf("%d", &number);
-	dec_bin(number);
-	//hex_oct();
-
+// Entry point
+	printf("OPERATION:\n");
+	printf("1) Translate from decimal to binary\n");
+	printf("2) Translate from hexadecimal to octal\n");
+	printf("\n");
+	int operation, dec_bin_Number;
+	char hex_oct_Number[20];
+// Choose operation and checkup
+	do {
+		printf("Enter number of operation:\n");
+		scanf("%d", &operation);
+		printf("\n");
+	} while (operation != 1 && operation != 2);
+// Output operation on screen
+	if (operation == 1) {
+		printf("Enter decimal number (no more than 1048576):\n");
+		scanf("%d", &dec_bin_Number);
+		dec_bin(dec_bin_Number);
+	} else if (operation == 2) {
+		int str = strlen(hex_oct_Number);
+		printf("Enter hexadecimal number (no more than 16^20):\n");
+		scanf(" %c", &hex_oct_Number);
+		printf("%c", hex_oct_Number[1]);
+		printf("%c", hex_oct_Number[2]);
+		//hex_oct(hex_oct_Number);
+	} else {
+		printf("FATAL ERROR!!!\n"); // Be on the safe side ;)
+	}
+	system("pause");
 	return 0;
 }
