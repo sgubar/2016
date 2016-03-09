b15cc51160fc2ad35369d16b2e515d7b9f132c1e
toporivskiy <toporivskie@gmail.com>
2016-03-09 19:38:32 +0200

    Some features ;)

--- dk52/toporivskiy/Lab1/Main.c
+++ dk52/toporivskiy/Lab1/Main.c
@@ -2,18 +2,23 @@
 //  Main.c
 //  Lab1
 //
-//  Created by Andrew Toporivskiy on 29/02/16.
+//  Created by Andrew Toporivskiy on 29/02/16 (DD/MM/YY).
 //  Copyright (C) 2016 Andrew Toporivskiy. All rights reserved.
 //
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#include "10to2.h"
-#include "16to8.h"
+#include "dec_bin.h"
+//#include "hex_oct.h"
 
-int main(int argc, const char * argv[])
+int main()
 {
-	do10to2();
+	int number;
+	printf("Enter decimal numer (no more than 1048576): \n");
+	scanf("%d", &number);
+	dec_bin(number);
+	//hex_oct();
 
 	return 0;
 }
