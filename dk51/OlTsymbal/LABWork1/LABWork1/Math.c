7a5e224782ce003bc2adc184a98100b3d763b839
12345 <sashko_tsumbal@mail.ru>
2016-03-09 17:52:48 +0200

    It`s a work version of my program. Maybe final version

--- dk51/OlTsymbal/LABWork1/LABWork1/Math.c
+++ dk51/OlTsymbal/LABWork1/LABWork1/Math.c
@@ -1,28 +1,41 @@
+//
+//  InputHEX.h
+//  Lab1
+//
+//  Created by Olexander Tsymbal on 02/28/16.
+//  Copyright © 2016 Olexander Tsymbal. All rights reserved.
+//
 #include<stdio.h>
 
 #include"Math.h"
-#include"InputHEX.h"
 
-int doInputHEX();
-int Sub(int numberB);
+int Sub(int numberHEX, int numberB);
 int Sum(int resultSub, int numberD);
 
-int Sub(int numberB) {
-	int resultSub = doInputHEX() - numberB;
-	printf("Sub=%d\n", resultSub);
-	return(resultSub);
+int Sub(int numberHEX, int numberB) //it is function of Sublimation (F-B)
+{
+	if (numberHEX != 0)//if numberHEX not equal 0(there is no more two symbols or it is not 0)
+	{
+		int resultSub = numberHEX - numberB;
+		/*printf("Sub=%d\n", resultSub);*/
+		return(resultSub);
+	}
+	else
+	{
+		printf("Invalid arguments. Check you entries\n");//if there is more than two symbols of HEXnumber
+		return 0;
+	}
 }
 
 
-
-int Sum(int resultSub, int numberD)
+int SumandMult(int resultSub, int numberD)//it is function of SUM(Sum(from A=1 to D Multiplication)
 {
 	int resultMult, result = 0;
 	for (int A=1 ; A <= numberD; A++)
 	{
-		resultMult = resultSub*A;
-		result += resultMult;
+		resultMult = resultSub*A;//Multiplication(Ax(F-B))
+		result += resultMult;//Sum(from A=1 to D Multiplication)
 	}
-	printf("RESULT=%d\n", result);
+	printf("RESULT=  %d\n", result);//output results
 	return(result);
 }
