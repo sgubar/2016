ad2d9db74a53ff72a958f7abe5f6474cd7bcafb3
bmaximovich <bmaximovich@gmail.com>
2016-03-10 23:03:41 +0200

    laba1

--- dk51/bmaximovich/laba1/laba1/formula.c
+++ dk51/bmaximovich/laba1/laba1/formula.c
@@ -1,16 +1,17 @@
 
 #include "Header.h"
 
-int fac(int n) {   //фактор≥ал числа n
+int fac(int n) {   //factorial of n
 	int k = 1;  
-	for (int i = 1; i <= n; i++) //поки ≥ буде менше або дор≥внювати n винонуЇтьс€ умова, ≥ зб≥льшуЇтьс€ на 1
-		k *= i; //поки ≥ менше n виконуЇтьс€ ц€ умова
+	for (int i = 1; i <= n; i++) //while i is less or equal n, it increases by 1
+
+		k *= i; //while "i" less then "n" this condition is done
 	return k;
 }
 
 double f(int a, int b, int c) {
 	double q = 0;
 	q += (double)(a*b) / (b + c*c);
-	for (int i = 0; i <= b; i++) q += fac(i); //на кожне повторенн€ циклу до q додаЇтьс€ значенн€ €ке повертаЇ функц≥€ fac
+	for (int i = 0; i <= b; i++) q += fac(i); // on each iteration of  cycle to "q" add value that returns fac function
 	return q;
 }
--- dk51/bmaximovich/laba1/laba1/main.c
+++ dk51/bmaximovich/laba1/laba1/main.c
@@ -9,16 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, c; //змінним присвоюємо тип данних типу int
-	printf("Enter A\n"); // виводимо повідомлення для вводу А
-	scanf_s("%d", &a); // зчитуємо  введені з клавіатури дані та вказуємо на змінну а. %d - десятична ціла типу int
+	int a, b, c;
+	printf("Enter A\n"); // displays a message "Enter A"
+	scanf_s("%d", &a); // read the data entered from the keyboard and pointing to variable "A"
 	printf("Enter B\n");
 	scanf_s("%d", &b);
 	printf("Enter C\n");
 	scanf_s("%d", &c);
-	if ((b + c*c) != 0)
+	if ((b + c*c) != 0) //checking for 0 in the denominator
 	{
-		double q = f(a, b, c); //тип числа з плаваючою точкою 
+		double q = f(a, b, c); //type of floating point numbers
 		printf("Result: %f\n", q);
 	}
 	else
