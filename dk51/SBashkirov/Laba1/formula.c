c8649cdfffe79bbaf0cefb384528dd5f7d72c966
Sbashkirov <stnslvbaskirov@gmail.com>
2016-03-10 18:16:46 +0200

    Lab 1 Update 2

--- dk51/SBashkirov/Laba1/formula.c
+++ dk51/SBashkirov/Laba1/formula.c
@@ -8,9 +8,6 @@ void calculate(int A, int B, int C)
 { 
 	if ((B + Pow2(C,2)) != 0)
 	{
-		printf("A*B = %d \n",(A*B));
-		printf("B + Pow2(C, 2) = %f\n", (B + Pow2(C, 2)));
-		printf("Summ(B) = %f\n", Summ(B));
 		double Result = ((A*B) / (B + Pow2(C, 2))) + Summ(B);
 		printf("Final result: %f\n", Result);
 	}
