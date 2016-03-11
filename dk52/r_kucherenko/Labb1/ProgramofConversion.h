0ea9e5c0a3ee0352c2503e1ed3318bde491d72ae
rkucherenko <k.ruslan.u@mail.ru>
2016-03-11 02:44:20 +0200

    Create ProgramofConversion.h

---
+++ dk52/r_kucherenko/Labb1/ProgramofConversion.h
@@ -0,0 +1,13 @@
+//
+//  ProgramofConversion.h
+//  lab1
+//
+//  Created by Ruslan Kucherenko on 6/03/16.
+//  Copyright (C) 2016 Ruslan Kucherenko. All rights reserved.
+//
+#ifndef ProgramofConversion_h
+#define ProgramofConversion_h
+
+void doProgramofConversion();
+
+#endif /* ProgramofConversion_h */
