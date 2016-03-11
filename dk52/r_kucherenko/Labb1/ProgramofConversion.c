a672fbc2a72d16b0b41ea888a26507498959c524
rkucherenko <k.ruslan.u@mail.ru>
2016-03-11 03:12:14 +0200

    Update ProgramofConversion.c

--- dk52/r_kucherenko/Labb1/ProgramofConversion.c
+++ dk52/r_kucherenko/Labb1/ProgramofConversion.c
@@ -5,7 +5,7 @@
 //  Created by Ruslan Kucherenko on 6/03/16.
 //  Copyright (C) 2016 Ruslan Kucherenko. All rights reserved.
 //
-// A8-A10 && A16-A10 work in folder "add"
+// A8->A10 && A16->A10 work in folder "add"
 
 
 #include <stdio.h>
