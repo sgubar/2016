b7ec713d4be089e0c0b1d88903301208d770cf99
sgubar <sl.gubar@ukr.net>
2016-03-13 19:23:30 +0200

    Correct behaviour with additional comments

--- dk51/AMahnyov/pointerDemo/getInput.c
+++ dk51/AMahnyov/pointerDemo/getInput.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include "getInput.h"
 
-int* getInput(int argNum);
-
-int* getInput(int argNum){
-    int answer[argNum];
-    int i;
-    for(i = 0; i < argNum; i++){
-        printf("Write an argument [%d]: ", i);
-        scanf("%d", &answer[i]);
-    }
-    return(answer);
+int *getInput(int argNum){
+//    int answer[argNum]; <! -- it is local variable, so it will be dead after go out from this function
+	
+	//The great solution is
+	int *answer = (int *)malloc(sizeof(int) * argNum);
+	
+	if (NULL != answer)
+	{
+		 for(int i = 0; i < argNum; i++)
+		 {
+			  printf("Write an argument [%d]: ", i);
+			//answer is pointer, so you can navigate through memory by use corresponding offset
+			//(answer + offset)
+			// so (answer + 0) is the same as (&answer[0])
+			// (anser + 1) == (&answer[1])
+			  scanf("%d", (answer + i));
+		 }
+	}
+	
+	return answer;
 }
--- dk51/AMahnyov/pointerDemo/main.c
+++ dk51/AMahnyov/pointerDemo/main.c
@@ -2,25 +2,34 @@
 #include <stdlib.h>
 #include "getInput.h"
 
-static void printInput(int *input);
+//static void printInput(int *input);
+static void printInput(int *input, int size); // you should use additional parameter that describes the size of array or use special symbol to mark the end of array - the same as in a string (char *)
 
 int main(){
     int argNum = 0;
     printf("How many arguments do you want?\n");
     scanf("%d", &argNum);
 
-    int *recievedAnswer;
+    int *recievedAnswer = NULL;
 
     recievedAnswer = getInput(argNum);
 
-    printInput(recievedAnswer);
+	 if (NULL != recievedAnswer)
+	 {
+	    printInput(recievedAnswer, argNum);
+	 
+		 //to avoid memory leak you should free memory
+		 free(recievedAnswer);
+	 }
 
     return 0;
 }
 
-void printInput(int *input){
+void printInput(int *input, int size){
     int i;
-    for(i = 0; i<sizeof(input); i++){
+// There are the bad solution, because you calculate the size of pointer nor the size of array (size of memory block)
+//    for(i = 0; i<sizeof(input); i++){
+		for (i = 0; i < size; i++){
         printf("You gave: [%d] as [%d]\n", i, input[i]);
     }
 }
