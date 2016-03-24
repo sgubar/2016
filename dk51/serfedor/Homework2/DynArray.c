dac61e48eba9b6f5401fd455de80af3c0847cb51
serfedor <sergeyfedorenko1997@gmail.com>
2016-03-24 23:56:57 +0200

    'TestHomeWork2'

---
+++ dk51/serfedor/Homework2/DynArray.c
@@ -0,0 +1,12 @@
+//
+//  DynArray.c
+//  Lab2
+//
+//  Created by Sergey Fedorenko on 3/23/16.
+//  Copyright © 2016 Sergey Fedorenko. All rights reserved.
+//
+#include "DynArray.h"
+DaInt (int aSize)
+{
+    printf ("%d", aSize);
+}
