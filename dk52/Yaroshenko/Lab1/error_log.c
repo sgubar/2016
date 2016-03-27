51a0bd90da6c4425d61cb3e80944210739aedb39
Yaroshenkom <gmaximum091@gmail.com>
2016-03-27 18:43:50 +0300

    Update and rename check.c to error_log.c

--- dk52/Yaroshenko/Lab1/error_log.c
+++ dk52/Yaroshenko/Lab1/error_log.c
@@ -2,22 +2,22 @@
 #include <conio.h>
 #include <stdlib.h> //exit
 #include <locale.h>
-#include "check.h"
-#include "read_and_prepare_hex_to_dec.h"
+#include "hex_converter.h"
 #include "start.h"
+#include "error_log.h"
 #include "countsigma.h"
 
-void errorer(int value) {
+void error_log(int value) {
 	switch (value)
 	{
 	case 1:
 		printf("\nD must be >= 1 \nRepeat input? y/n ");
 		if (_getch() == 121) {
-			printf("Enter decimal D: "); scanf_s("%d", &D); if (D <= 1) are_u_dumb(1); break;
+			printf("Enter decimal D: "); scanf_s("%d", &D); if (D <= 1) error_log(1); break;
 		};
 		exit(1);
 	case 2:
-		printf("\nerr input HEX digit\n"); exit(1);
+		printf_s("\nerr input HEX digit\n"); exit(1);
 
 
 		exit(1);
