9eec360eba1ba650193cdd36a9df494e61c3b6b0
STimoshenko <vendetta16@ukr.net>
2016-03-27 18:35:23 +0300

    Home Work 1

---
+++ dk51/STimoshenko/HW1/ConsoleApplication3/main.c
@@ -0,0 +1,69 @@
+//
+//  main.c
+//  demoList
+//
+//  Created by Slava Gubar on 3/1/16.
+//  Copyright © 2016 Slava Gubar. All rights reserved.
+//
+
+#include <stdio.h>
+#include "SingleLinkedNode.h"
+#include "SingleLinkedList.h"
+
+void doTestSLList();
+void doPrintSLList(const IntList *aList);
+
+int main(int argc, const char * argv[])
+{
+	doTestSLList();
+
+	return 0;
+}
+
+void doTestSLList()
+{
+	printf("Start to demo a single-linked list ...\n");
+	
+	IntNode *theNode2 = SLCreateNodeWithIntValue(2);
+	
+	IntNode *theNode3 = SLCreateNodeWithIntValue(3);//Add a new node
+
+	IntNode *theNode4 = SLCreateNodeWithIntValue(4);
+
+	IntList *theList = SLCreateList();
+	printf("number of elements: %d\n", SLCountList(theList));
+
+
+	SLAddNode(theList, theNode2);
+	printf("number of elements: %d\n", SLCountList(theList));
+
+	SLAddNode(theList, theNode3);
+	printf("number of elements: %d\n", SLCountList(theList));
+
+
+	SLAddNode(theList, theNode4);
+	printf("number of elements: %d\n", SLCountList(theList));
+	
+	doPrintSLList(theList);
+
+	SLRemovedNodeAtIndex(theList, 1);//delete a new node
+	printf("'theNode3' is deleted\n");
+
+	doPrintSLList(theList);
+	SLFreeList(theList);
+
+
+}
+
+void doPrintSLList(const IntList *aList)
+{
+	for (int i = 0; i < SLCountList(aList); i++)
+	{
+		IntNode *theNode = SLNodeAtIndex(aList, i);
+		if (NULL != theNode)
+		{
+			printf("node[%d].value = %d\n", i, theNode->value);
+		}
+	}
+	system("pause");
+}
