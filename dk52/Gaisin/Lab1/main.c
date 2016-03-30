13f5709a4c30f72797099b057a6b829a02801967
Artem-gaus <gausin.artem@gmail.com>
2016-03-30 17:29:56 +0300

    Update main.c

--- dk52/Gaisin/Lab1/main.c
+++ dk52/Gaisin/Lab1/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include <math.h>/*mathematical library*/
 
-#include "sum.h"
+#include "sum.h"/*this three library it header files when conected "main.c" with other ".c" files*/
 #include "csqrt.h"
 #include "fact.h"
 
 int main ()
 {
-	int q, w, fac, pt, b, sum;
+	int q, w, fac, pt, b, sum; /*declarations variables*/
 	printf("enter a\n");
 	scanf("%i", &w);
-	int theFact = dofact(fac);
+	int theFact = dofact(fac);/*appropriation variables theFact and other, value "fac" ...*/
 	int theCsqrt = docsqrt(sqrt(pt) / b);
 	int theSum = dosum(sum);
 	q = w * theCsqrt + theSum * theFact;
