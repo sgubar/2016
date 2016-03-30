b678a5b01e8ddc87cf7c6092ba7046aace898a06
Artem-gaus <gausin.artem@gmail.com>
2016-03-30 17:48:22 +0300

    Update sum.c

--- dk52/Gaisin/Lab1/sum.c
+++ dk52/Gaisin/Lab1/sum.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "sum.h"
+#include "sum.h"/*conect by using header file*/
 
 int dosum()
 {
 	int p, e, sum = 1;
 	printf("enter again c\n");
 	scanf("%i", &e);
-	for (p = 0; p < (e + 1); p++)
+	for (p = 0; p < (e + 1); p++)/*cycle counting between 1 to (a+1) and adds sum until p < (e+1)*/
 	{
 		sum += p;
 	}
 	printf("sum = %i\n", sum);
-	return sum;
+	return sum;/*return result "sum"*/
 }
