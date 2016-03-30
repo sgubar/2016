e82ba8d1a368d095d80e4416489041aced935faa
Artem-gaus <gausin.artem@gmail.com>
2016-03-30 18:50:15 +0300

    Update fact.c

--- dk52/Gaisin/Lab1/fact.c
+++ dk52/Gaisin/Lab1/fact.c
@@ -7,10 +7,17 @@ int dofact()
 	int i, fac = 1, a;
 	printf("enter again a\n");
 	scanf("%i", &a);
+	if(a > 0)
+	{
 	for (i = 1; i < (a + 1); i++) /*cycle counting between 1 to (a+1) and multiplies i*/
 	{
 		fac *= i;
 	}
 	printf("fac = %i\n", fac);
 	return fac;
+	}
+	else
+	{
+		printf("error\n");
+	}
 }
