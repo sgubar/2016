8777decc68c290d5aacefc9596edbda325d1d47e
IrBevza <bevira@ukr.net>
2016-04-01 11:39:41 +0300

    Update sum.c

--- dk52/Bevza/lab1/sum.c
+++ dk52/Bevza/lab1/sum.c
@@ -15,14 +15,16 @@ int SUM1 ()
 	int A=0, B;
 	printf("Enter A\n");
 	scanf("%i", &A);
-	SUM1=A*A+B*B;
+	sum1=A*A+B*B;
+return SUM1;
 }
 int SUM2 ()
 {
-	SUM2=B+PowC;
-	if (SUM2==0){
+	sum2=B+PowC;
+	if (sum2==0){
 		printf("Error. SUM2=0\n");
 	}
+return SUM2;
 }
 
 
