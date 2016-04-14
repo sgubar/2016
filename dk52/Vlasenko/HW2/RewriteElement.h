c9eee2e3d250b2a8a6c2248b1e68090a73552a9e
oxyyyyy <elektronikaion@gmail.com>
2016-04-14 17:15:37 +0300

    Added files via upload

---
+++ dk52/Vlasenko/HW2/RewriteElement.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// The function of writing value at index.
+
+void RewriteElement(int *dynarray)
+{
+	int target_indx, target_value;
+
+	printf("Write an index: ");
+	scanf_s("%d", &target_indx);
+	printf("Write a value: ");
+	scanf_s("%d", &target_value);
+
+	dynarray[target_indx - 1] = target_value;
+}
