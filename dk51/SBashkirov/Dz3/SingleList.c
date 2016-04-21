1d1a2908903b2186a42bf13fbaab1d338b7c4d12
Sbashkirov <stnslvbaskirov@gmail.com>
2016-04-21 23:06:36 +0300

    Dz3 Done

--- dk51/SBashkirov/Dz3/SingleList.c
+++ dk51/SBashkirov/Dz3/SingleList.c
@@ -62,6 +62,7 @@ FloatNode *NodeAdd(FloatList *aList, float aValue)
 	else
 	{
 		FloatNode *theTail = aList->tail;
+		aNewNode->prevNode = theTail;
 		aList->tail = aNewNode;
 
 		if (NULL != theTail)
@@ -126,6 +127,7 @@ FloatNode *InsertNodeAtIndex(FloatList *aList, FloatNode *aNewNode, int anIndex)
 		if (0 == anIndex)
 		{
 			aNewNode->nextNode = aList->head;
+			aNewNode->nextNode->prevNode = aNewNode;
 			aList->head = aNewNode;
 			aList->count += 1;
 		}
@@ -134,7 +136,9 @@ FloatNode *InsertNodeAtIndex(FloatList *aList, FloatNode *aNewNode, int anIndex)
 			FloatNode *PrevNode = NodeAtIndex(aList, anIndex - 1);
 			if (anIndex == aList->count + 1) aList->tail = aNewNode;
 			else aNewNode->nextNode = PrevNode->nextNode;
+			aNewNode->prevNode = PrevNode;
 			PrevNode->nextNode = aNewNode;
+			aNewNode->nextNode->prevNode = aNewNode;
 			aList->count += 1;
 		}
 		return(aNewNode);
@@ -151,16 +155,17 @@ FloatNode *RemovedNodeAtIndex(FloatList *aList, int anIndex)
 		FloatNode *RemovedNode = NodeAtIndex(aList, anIndex);
 		if (0 == anIndex)
 		{
-			aList->head = NodeAtIndex(aList, anIndex + 1);
+			aList->head = RemovedNode->nextNode;
+			RemovedNode->nextNode->prevNode = NULL;
 			aList->count -= 1;
 		}
 		else
 		{
-			FloatNode *PrevNode = NodeAtIndex(aList, anIndex - 1);
-			if (anIndex == aList->count) aList->tail = PrevNode;
+			FloatNode *PrevNode =RemovedNode->prevNode;
 			PrevNode->nextNode = RemovedNode->nextNode;
+			if (anIndex == aList->count-1) aList->tail = PrevNode;
+			else RemovedNode->nextNode->prevNode = PrevNode;
 			aList->count -= 1;
-			
 		}
 		return(RemovedNode);
 	}
@@ -219,21 +224,20 @@ FloatList *deleteKeys(FloatList *aList, float key1, float key2, float key3)
 //Dz3
 void insertionSort(FloatList *aList)
 {
-	for (int i = aList->count - 1;0 != i;i--) 
+	int RemIndex = 1;
+	for (int i = 1;i < aList->count;i++)
 	{
-		FloatNode *UnSortNode = NodeAtIndex(aList, i);
-		FloatNode *NextNode = UnSortNode->nextNode;
-		int index = i + 1;
-		while (NULL != NextNode)
+		FloatNode *UnSortNode = RemovedNodeAtIndex(aList, RemIndex);
+		FloatNode *PrevNode = UnSortNode->prevNode;
+		int PasteIndex = RemIndex;
+		while (PrevNode != NULL)
 		{
-			index++;
-			if (NextNode->value < UnSortNode->value)
-			{
-				InsertNodeAtIndex(aList, RemovedNodeAtIndex(aList,i), index);
-				break;
-			}
-			NextNode = NextNode->nextNode;
+			if (UnSortNode->value > PrevNode->value) break;
+			PrevNode = PrevNode->prevNode;
+			if (PasteIndex != 0) PasteIndex--;
 		}
-
+		InsertNodeAtIndex(aList, UnSortNode, PasteIndex);
+		RemIndex++;
 	}
+	printf("RemIndex = %d\n", RemIndex);
 }
