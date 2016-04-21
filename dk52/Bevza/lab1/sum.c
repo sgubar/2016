cfb1770d84be8ed705bcf5e020e0313c8c811b31
IrBevza <bevira@ukr.net>
2016-04-21 21:23:02 +0300

    Update sum.c

--- dk52/Bevza/lab1/sum.c
+++ dk52/Bevza/lab1/sum.c
@@ -10,22 +10,18 @@
 #include "sum.h"
 #include "powC.h"
 
-int SUM1 ()
+int SUM1 () /*initialization of function "SUM1"*/
 {
-	int A=0, B;
-	printf("Enter A\n");
-	scanf("%i", &A);
-	sum1=A*A+B*B;
-return SUM1;
+	int A=0, B; /*a temporary variable initialization*/
+	printf("Enter A\n"); /*print at screen "Enter A"*/
+	scanf("%i", &A); /*readout value of variable "A"*/
+	sum1=A*A+B*B; /*calculating the sum of A squared and B squared*/
+	return SUM1;
 }
-int SUM2 ()
+int SUM2 () /*initialization of function "SUM2"*/
 {
-	sum2=B+PowC;
+	sum2=B+PowC; /*calculating the sum of variable B and function "PowC"*/
 	if (sum2==0){
-		printf("Error. SUM2=0\n");
+		printf("Error. SUM2=0\n"); /*Checing the value of SUM2. Print at screen "Error. SUM2=0" if value of function "SUM2" is 0*/
 	}
-return SUM2;
-}
-
-
-
+	return SUM2;
