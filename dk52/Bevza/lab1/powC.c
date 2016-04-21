e428dda5e040e5abb3b527b402199075f0dd1d1f
IrBevza <bevira@ukr.net>
2016-04-21 21:20:22 +0300

    Update powC.c

--- dk52/Bevza/lab1/powC.c
+++ dk52/Bevza/lab1/powC.c
@@ -8,11 +8,11 @@
 //
 #include <stdio.h>
 #include "powC.h"
-int PowC()
+int PowC() /*initialization of function "PowC"*/
 {
-	int C=0, y=0, powC;
-	printf("Enter C\n");
-	scanf("%i", &C);
+	int C=0, y=0, powC; /*a temporary variable initialization*/
+	printf("Enter C\n"); /*print at screen "Enter C"*/
+	scanf("%i", &C);/*readout value of variable "C"*/
 	powC=1;
 	y=0;
 	do {
@@ -20,7 +20,6 @@ int PowC()
 	y++;
 }
 	while (y!=C);
-	printf("PowC=%i\n", powC);
-	return PowC;
+	printf("PowC=%i\n", powC);/*print at screen value of "PowC"*/
+	return PowC; /*calculating the value of "PowC" by doing cycle: multiply value of "FactorialB" and 5 C times*/
 }
-
