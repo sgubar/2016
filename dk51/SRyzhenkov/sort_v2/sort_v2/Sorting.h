48398c6c4aca613449a037754c31e1d41551d337
Serhii <Sergiy905@gmail.com>
2016-04-22 14:25:32 +0300

    upd copyright

--- dk51/SRyzhenkov/sort_v2/sort_v2/Sorting.h
+++ dk51/SRyzhenkov/sort_v2/sort_v2/Sorting.h
@@ -1,3 +1,9 @@
+//  Sorting.h
+//  sort_v2
+//
+//  Created by Ryzhenkov Serhii on 21/04/16.
+//  Copyright © 2016 Ryzhenkov Serhii. All rights reserved.
+
 #ifndef Sorting_h
 #define Sorting_h
 
--- dk51/SRyzhenkov/sort_v2/sort_v2/main.c
+++ dk51/SRyzhenkov/sort_v2/sort_v2/main.c
@@ -1,22 +1,44 @@
+//  main.c
+//  sort_v2
+//
+//  Created by Ryzhenkov Serhii on 21/04/16.
+//  Copyright © 2016 Ryzhenkov Serhii. All rights reserved.
+
+#define sizeArray (20000)
 #include <stdio.h>
 #include "Sorting.h"
+
 
 void printArray(int anArray[], int aSize);
 int *CreateArray(int Size);
 
 int main()
 {
-	int sizeArray = 1000;
-	//printf_s("SizeArray: \n", &sizeArray);
-	//scanf_s("10 = ", &sizeArray);
-	//printf_s("SizeArray: \n", &sizeArray);
+	
+	//printf("SizeArray:  %d \n", sizeArray);
+	//scanf("Size = %d \n", &sizeArray);
+	//printf("SizeArray: %d \n", sizeArray);
 
-	int *testArray = CreateArray(sizeArray);
+	int *SortArray = CreateArray(sizeArray);
+	int bubArray[sizeArray];
+	int SelArray[sizeArray];
+	int InsArray[sizeArray];
+	int ShellArray[sizeArray];
+	for (int z = 0; z < sizeArray; z++) {
+		bubArray[z] = SortArray[z];
+		SelArray[z] = SortArray[z];
+		InsArray[z] = SortArray[z];
+		ShellArray[z] = SortArray[z];
+	}
 
-	bubbleSort(testArray, sizeArray);
-	selectionSort(testArray, sizeArray);
-	insertionSort(testArray, sizeArray);
-	shellSort(testArray, sizeArray);
+	//printArray(bubArray, sizeArray);
+	bubbleSort(bubArray, sizeArray);
+	//printArray(bubArray, sizeArray);
+
+	selectionSort(SelArray, sizeArray);
+
+	insertionSort(InsArray, sizeArray);
+	shellSort(ShellArray, sizeArray);
 
 	system("pause");
 }
