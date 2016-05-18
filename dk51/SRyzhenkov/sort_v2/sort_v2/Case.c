40aef62432fcdaa5be2c43af31addeafe029743a
Sryzhenkov <Sergiy905@gmail.com>
2016-05-18 23:49:33 +0300

    Laba3_The_End

--- dk51/SRyzhenkov/sort_v2/sort_v2/Case.c
+++ dk51/SRyzhenkov/sort_v2/sort_v2/Case.c
@@ -8,9 +8,12 @@
 #define SIZE_ARRAY_1 (1000)
 #define SIZE_ARRAY_2 (5000)
 #define SIZE_ARRAY_3 (10000)
-
+#include "case.h"
+#include <time.h>
+#include <stdio.h>
 
 int Sorting_500() {
+	FILE * ptr = fopen("Plot.csv", "a+");
 
 	int arrSize;
 	int *SortArray = CreateArray(SIZE_ARRAY_0);
@@ -18,12 +21,16 @@ int Sorting_500() {
 	int SelArray[SIZE_ARRAY_0];
 	int InsArray[SIZE_ARRAY_0];
 	int ShellArray[SIZE_ARRAY_0];
+	int QuickArray[SIZE_ARRAY_0];
+	int QuickArray_v2[SIZE_ARRAY_0];
 
 	for (int z = 0; z < SIZE_ARRAY_0; z++) {
 		bubArray[z] = SortArray[z];
 		SelArray[z] = SortArray[z];
 		InsArray[z] = SortArray[z];
 		ShellArray[z] = SortArray[z];
+		QuickArray[z] = SortArray[z];
+		QuickArray_v2[z] = SortArray[z];
 	}
 
 	//printArray(bubArray, sizeArray);
@@ -34,9 +41,34 @@ int Sorting_500() {
 
 	insertionSort(InsArray, SIZE_ARRAY_0);
 	shellSort(ShellArray, SIZE_ARRAY_0);
+
+	clock_t start_t5, end_t5, start_t6, end_t6;
+
+	start_t5 = clock();
+
+	quickSort(QuickArray, 0, 499);
+
+	end_t5 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t5);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t5);
+
+	printf("time of quickSort: %.3lf\n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_1; %.3lf; \n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+	
+	start_t6 = clock();
+	quickSort_v2(QuickArray_v2, 0, 499);
+	end_t6 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t6);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t6);
+
+	printf("time of quickSort_v2: %.3lf\n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_2; %.3lf; \n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fclose(ptr);
+
 }
 
 int Sorting_1000() {
+	FILE * ptr = fopen("Plot.csv", "a+");
 
 	int arrSize;
 	int *SortArray = CreateArray(SIZE_ARRAY_1);
@@ -44,12 +76,16 @@ int Sorting_1000() {
 	int SelArray[SIZE_ARRAY_1];
 	int InsArray[SIZE_ARRAY_1];
 	int ShellArray[SIZE_ARRAY_1];
+	int QuickArray[SIZE_ARRAY_1];
+	int QuickArray_v2[SIZE_ARRAY_1];
 
 	for (int z = 0; z < SIZE_ARRAY_1; z++) {
 		bubArray[z] = SortArray[z];
 		SelArray[z] = SortArray[z];
 		InsArray[z] = SortArray[z];
 		ShellArray[z] = SortArray[z];
+		QuickArray[z] = SortArray[z];
+		QuickArray_v2[z] = SortArray[z];
 	}
 
 	//printArray(bubArray, sizeArray);
@@ -60,9 +96,32 @@ int Sorting_1000() {
 
 	insertionSort(InsArray, SIZE_ARRAY_1);
 	shellSort(ShellArray, SIZE_ARRAY_1);
+
+	clock_t start_t5, end_t5, start_t6, end_t6;
+
+	start_t5 = clock();
+	quickSort(QuickArray, 0, 999); 
+	end_t5 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t5);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t5);
+
+	printf("time of quickSort: %.3lf\n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_1; %.3lf; \n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+	
+
+	start_t6 = clock();
+	quickSort_v2(QuickArray_v2, 0, 999);
+	end_t6 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t6);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t6);
+
+	printf("time of quickSort: %.3lf\n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_2; %.3lf; \n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fclose(ptr);
 }
 
 int Sorting_5000() {
+	FILE * ptr = fopen("Plot.csv", "a+");
 
 	int arrSize;
 	int *SortArray = CreateArray(SIZE_ARRAY_2);
@@ -70,12 +129,16 @@ int Sorting_5000() {
 	int SelArray[SIZE_ARRAY_2];
 	int InsArray[SIZE_ARRAY_2];
 	int ShellArray[SIZE_ARRAY_2];
+	int QuickArray[SIZE_ARRAY_2];
+	int QuickArray_v2[SIZE_ARRAY_2];
 
 	for (int z = 0; z < SIZE_ARRAY_2; z++) {
 		bubArray[z] = SortArray[z];
 		SelArray[z] = SortArray[z];
 		InsArray[z] = SortArray[z];
 		ShellArray[z] = SortArray[z];
+		QuickArray[z] = SortArray[z];
+		QuickArray_v2[z] = SortArray[z];
 	}
 
 	//printArray(bubArray, sizeArray);
@@ -86,9 +149,32 @@ int Sorting_5000() {
 
 	insertionSort(InsArray, SIZE_ARRAY_2);
 	shellSort(ShellArray, SIZE_ARRAY_2);
+
+	clock_t start_t5, end_t5, start_t6, end_t6;
+
+	start_t5 = clock();
+	quickSort(QuickArray, 0, 4999);
+	end_t5 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t5);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t5);
+
+	printf("time of quickSort: %.3lf\n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_1; %.3lf; \n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+
+
+	start_t6 = clock();
+	quickSort_v2(QuickArray_v2, 0, 4999);
+	end_t6 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t6);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t6);
+
+	printf("time of quickSort: %.3lf\n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_2; %.3lf; \n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fclose(ptr);
 }
 
 int Sorting_10000() {
+	FILE * ptr = fopen("Plot.csv", "a+");
 
 	int arrSize;
 	int *SortArray = CreateArray(SIZE_ARRAY_3);
@@ -96,12 +182,16 @@ int Sorting_10000() {
 	int SelArray[SIZE_ARRAY_3];
 	int InsArray[SIZE_ARRAY_3];
 	int ShellArray[SIZE_ARRAY_3];
+	int QuickArray[SIZE_ARRAY_3];
+	int QuickArray_v2[SIZE_ARRAY_3];
 
 	for (int z = 0; z < SIZE_ARRAY_3; z++) {
 		bubArray[z] = SortArray[z];
 		SelArray[z] = SortArray[z];
 		InsArray[z] = SortArray[z];
 		ShellArray[z] = SortArray[z];
+		QuickArray[z] = SortArray[z];
+		QuickArray_v2[z] = SortArray[z];
 	}
 
 	//printArray(bubArray, sizeArray);
@@ -112,4 +202,36 @@ int Sorting_10000() {
 
 	insertionSort(InsArray, SIZE_ARRAY_3);
 	shellSort(ShellArray, SIZE_ARRAY_3);
+	clock_t start_t5, end_t5, start_t6, end_t6;
+
+	start_t5 = clock();
+	quickSort(QuickArray, 0, 9999);
+	end_t5 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t5);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t5);
+
+	printf("time of quickSort: %.3lf\n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_1; %.3lf; \n", (double)(end_t5 - start_t5) / CLOCKS_PER_SEC);
+
+
+	start_t6 = clock();
+	quickSort_v2(QuickArray_v2, 0, 9999);
+	end_t6 = clock();
+	printf("Going to scan a big loop, start_t = %ld\n", start_t6);
+	printf("Going to scan a big loop, start_t = %ld\n", end_t6);
+
+	printf("time of quickSort: %.3lf\n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fprintf(ptr, "Швидке сортування_2; %.3lf; \n", (double)(end_t6 - start_t6) / CLOCKS_PER_SEC);
+	fclose(ptr);
+}
+
+void printArray(int anArray[], int aSize)
+{
+	printf("{");
+	for (int i = 0; i < aSize; i++)
+	{
+		printf(" %d ", anArray[i]);
+	}
+
+	printf("}\n");
 }
--- dk51/SRyzhenkov/sort_v2/sort_v2/Sorting.h
+++ dk51/SRyzhenkov/sort_v2/sort_v2/Sorting.h
@@ -14,5 +14,8 @@ void selectionSort(int anArray[], int aSize);
 void insertionSort(int anArray[], int aSize);
 void shellSort(int anArray[], int aCount);
 
+int partitionIt(int anArray[], int aLeftIndex, int aRightIndex, int aPivot);
+void quickSort(int anArray[], int aLeftIndex, int aRightIndex);
+void quickSort_v2(int anArray[], int aLeftIndex, int aRightIndex);
 
 #endif /*Sorting_h */
