1538da67da0fa1ba8d5193eb377f71168126f4d8
Dmitry <dima.net_98@mail.ru>
2016-05-20 01:16:25 +0300

    Add files via upload

---
+++ dk52/Sakharov/Lab3/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Dmitry Sakharov
+ *
+ * Created on 9 травня 2016, 0:00
+ * Compiled with gcc(cygwin)
+ */
+
+#include <stdio.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
