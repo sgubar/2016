e4cdad925b7e5d2f080377e4e22857c7391bc988
Alexandr Molochko <sa74111@rambler.ru>
2016-05-20 12:55:27 +0300

    Add files via upload

---
+++ dk52/Amolochko/lab3/SimpleSort.h
@@ -0,0 +1,10 @@
+#ifndef SimpleSort_h
+#define SimpleSort_h
+
+#include <stdio.h>
+
+void bubbleSort(int anArray[], int aSize);
+void selectionSort(int anArray[], int aSize);
+void insertionSort(int anArray[], int aSize);
+
+#endif
