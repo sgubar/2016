232d31529d1d80e6c31746f9c58585e548c09be1
nbronnikov <nazar_9898@mail.ru>
2016-05-20 05:47:06 +0300

    Create DynamicArray.h

---
+++ dk51/nbronnikov/hw2/DynamicArray.h
@@ -0,0 +1,27 @@
+//
+//  DynamicArray.h
+//  doTest
+//
+//  Created by Nazar Bronnikov on 3/24/16.
+//  Copyright © 2016 Nazar Bronnikov. All rights reserved.
+//
+
+#ifndef DynamicArray_h
+#define DynamicArray_h
+
+#include <stdio.h>
+
+typedef struct __dynamicIntArray
+{
+	int *storage;
+	int physicalSize;
+	int logicalSize;
+}DynamicIntArray;
+
+DynamicIntArray *createDAInt(int aSize);
+void freeDAInt(DynamicIntArray *anArray);
+
+int valueAtIndex(DynamicIntArray *anArray, int anIndex);
+void setValueAtIndex(DynamicIntArray *anArray, int anValue, int anIndex);
+
+#endif /* DynamicArray_h */
