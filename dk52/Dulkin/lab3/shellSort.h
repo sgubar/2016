4074b842a7ad639968b0d2bb80150d4a1ae6a378
olexore <olexore@ukr.net>
2016-05-20 04:07:55 +0300

    Update shellSort.h

--- dk52/Dulkin/lab3/shellSort.h
+++ dk52/Dulkin/lab3/shellSort.h
@@ -1,4 +1,11 @@
-#ifndef shellSort_h
+/* 
+ * File:   shellSort.h
+ * Author: Dulkin Oleg
+ *
+ * Created on 19 квітня 2016, 4:04
+ * Compiled with gcc(cygwin)
+ */
+ifndef shellSort_h
 #define shellSort_h
 
 #include <stdio.h>
