f1f39b471bf3cbea983c0de9c8d2bb12ea27a9e1
olexore <olexore@ukr.net>
2016-05-20 04:06:13 +0300

    Update node.h

--- dk52/Dulkin/lab2/node.h
+++ dk52/Dulkin/lab2/node.h
@@ -1,4 +1,11 @@
-#ifndef SingleNode_h
+/* 
+ * File:   node.h
+ * Author: Dulkin Oleg
+ *
+ * Created on 19 квітня 2016, 4:04
+ * Compiled with gcc(cygwin)
+ */
+ifndef SingleNode_h
 #define SingleNode_h
 
 #include <stdio.h>
