280772fdb0f998830a3ab17c2074d023a11d1745
Yaroshenkom <gmaximum091@gmail.com>
2016-05-25 20:56:26 +0300

    Update main.c

--- dk52/Yaroshenko/HW1/main.c
+++ dk52/Yaroshenko/HW1/main.c
@@ -13,10 +13,10 @@ int main() {
 	printf("You have the list of random 10 numbers\n");
 	
 	
-	head=PrintRandomList();                                               //create list of random 10 number
+	head=PrintRandomList();                                               //создаем список из 10 случайных чисел
 	printf("\nWhat is index of a new node?\n");
 	scanf("%d",&n);
-	if(n<1 || n>11){                                                      //checking index
+	if(n<1 || n>11){                                                      //проверяем корректность введенного индекса
 	  printf("\nYou pick the wrong index!\n");
 	  system("pause");
 	  exit(0);
@@ -25,16 +25,16 @@ int main() {
 	
 	printf("What value would you like to insert?\n");
 	scanf("%d",&value);
-	newhead=AddNodeToList(head, n, value);                                //insertion of the node
+	newhead=AddNodeToList(head, n, value);                                //добавляем ноду
 	
 	printf("What is the index of node you would like to remove?\n");      
 	scanf("%d",&m);
-	if(m<1 || m>12){                                                      //checking index
+	if(m<1 || m>12){                                                      //проверяем корректность введенного индекса
 	  printf("You pick the wrong index!\n");
 	  system("pause");
 	  exit(0);
 	}
-	removed=RemoveNode(newhead,m);                                        //removing node
+	removed=RemoveNode(newhead,m);                                        //извлекаем ноду из списка, но не удаляем его
 	printf("\nYou removed %d ", removed->number);
 	
 	
