252a1298367d53e05f6faac0a7a6b54f31ea0b9d
olexore <olexore@ukr.net>
2016-06-02 23:21:33 +0300

    Update and rename csqrt_ratail.h to drob.h

--- dk52/Dulkin/lab1/drob.h
+++ dk52/Dulkin/lab1/drob.h
@@ -1,14 +1,13 @@
-
 /* 
  * File:   csqrt_ratail
  * Author: Dulkin Oleg
  *
- * Created on 19 травня 2016, 4:01
+ * Created on 19 ?????? 2016, 4:01
  */
 
 #ifndef RETAIL_H
 #define RETAIL_H
 
-float csqrt_ratail(float a, float b, float c);
+float drob(float a, float b, float c);
 
 #endif
