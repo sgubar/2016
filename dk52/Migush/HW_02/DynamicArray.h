1dd0cc2e9173714ccdd60b5df010f533b7938d1c
mmigush <maxic9898@gmail.com>
2016-06-26 23:33:40 +0300

    Create DynamicArray.h

---
+++ dk52/Migush/HW_02/DynamicArray.h
@@ -0,0 +1,30 @@
+//
+//  DynamicArray.h
+//  doTest
+//
+//  Created by Slava Gubar on 3/24/16.
+//  Copyright Â© 2016 Slava Gubar. All rights reserved.
+//
+
+#ifndef DynamicArray_h
+#define DynamicArray_h
+
+#include <stdio.h>
+
+typedef struct __dynamicIntArray
+{
+	int *storage;
+	int physicalSize;
+	int logicalSize;
+	
+}DynamicIntArray;
+
+DynamicIntArray *createDAInt(int aSize);
+void freeDAInt(DynamicIntArray *anArray);
+
+int valueAtIndex(int anIndex,DynamicIntArray *anArray);
+void setValueAtIndex(int anValue, int anIndex,DynamicIntArray *anArray);
+
+
+int infofunc(DynamicIntArray *anArray);
+#endif /* DynamicArray_h */
