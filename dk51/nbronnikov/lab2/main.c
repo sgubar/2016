816aac5343df33f37d694df7f9f4d1c57c6ee096
nbronnikov <nazar_9898@mail.ru>
2016-06-26 23:51:25 +0300

    Update main.c

--- dk51/nbronnikov/lab2/main.c
+++ dk51/nbronnikov/lab2/main.c
@@ -7,182 +7,72 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#include "list.h"
-
-
-void show_menu (int *choise);
-void generate_list (int list_size);
-
+#include "List.h"
+#include "Node.h"
+void doPrintList(const List *aList);
+void Sorting(int Array[5]);
 int main(void)
 {
-	list_init();
-	
-	int choise = -1;
-	int exit_val = 1;
-	
-    while (exit_val) 
-    {
-		show_menu(&choise);
-		
-		printf("\n");
+	int count = 0;
+	int anArray[5];
+		List *theList = doList();
+		doPrintList(theList);
+		IntNode *minNode = findMinNode(theList);
+		int minimum = minNode->value;
+		IntNode *maxNode = findMaxNode(theList);
+		int maximum = maxNode->value;
+		printf("Min node: %d\nMax node: %d\n", minimum, maximum);
+		int swap = minNode->value;
+		minNode->value = maxNode->value;
+		maxNode->value = swap;
+		printf("Swap min and max in list\n");
+		doPrintList(theList);
+		printf("List after sorting\n");
+		Sorting(theList, anArray);
+		system("pause");
 
-		switch (choise)
-		{
-			case 0:
-			{
-				int size = 0;
-
-				printf("Razmer spiska:  ");
-
-				scanf_s("%d%*[^\n] ", &size);
-
-				generate_list(size);
-
-				list_print();
-
-				printf("\n \n \n");
-			}
-			break;
-			
-			case 1:
-			case 2:
-			{
-				if (list_sort(choise))
-				{
-					printf("\n \n Oshibka pri sortirovke. \n \n");
-					
-					break;
-				}
-				
-				list_print();
-				
-				printf("\n \n \n");
-			}
-			break;
-			
-			case 3:
-			{
-				if (list_swap_maxmin())
-				{
-					printf("\n \n V spiske odin element \n \n");
-					
-					break;
-				}
-				
-				list_print();
-				
-				printf("\n \n \n");
-				
-			}
-			break;
-
-			case 4:
-			{
-				printf(" \n Vvedite klyuch:  ");
-				
-				int key = 0;
-				
-				scanf_s("%d%*[^\n] ", &key);
-				
-				int res = list_del_elem(key);
-
-				if (res == 0)
-				{
-					list_print();
-				}
-				if (res == 1)
-				{
-					printf("Klyuch ne nayden \n");
-				}
-				else if (res == 2)
-				{
-					printf("Spisok pustoy \n");
-
-					break;
-				}
-				
-				printf("\n \n \n");
-			}
-			break;
-
-			case 5:
-			{
-				// todo manual adding elements to the list
-				
-				list_print();
-				
-				printf("\n \n \n");
-				
-				while (list_get_size() >= 3)
-				{
-					list_del_elem(3);
-					
-					list_print();
-					
-					printf("\n \n \n");
-				}
-			}
-			break;
-			
-			case 9:
-			{
-				exit_val = 0;
-			}
-			break;
-			
-			default:
-			{
-				printf("Nevernaya komanda \n");
-			}
-			break;
-		}
-    }
-	
-	return 0;
 }
-
-void show_menu (int *choise)
+void doPrintList(const List *aList)
 {
-	printf("Vvedite nomer komandu: \n");
 	
-	printf("\t Generatciya spiska - 0. \n");
-	
-	printf("\t Sortirovka po vozrostaniy - 1. \n");
-	printf("\t Sortirovka po spadaniy - 2. \n");
-	
-	printf("\t Pomenyat mestami max i min elementu - 3. \n");
-	
-	printf("\t Udalit element po klyuchu - 4. \n");
-	
-	printf("\t Udalit kazhduy 3y element - 5. \n");
-	
-	printf("\t Vuhod is programmu - 9. \n");
-	
-	printf("\n Vash vubor: ");
-
-	scanf_s("%d%*[^\n] ", choise);
-}
-
-void generate_list (int list_size)
-{
-	srand((unsigned int)time(NULL));
-	
-	int value = 0;
-	
-	printf("\n");
-
-	list_init();
-
-	for (int i = 0; i < list_size; i++)
+	for (int i = 0; i<CountList(aList); i++)
 	{
-		value = rand() % 1000;
-		
-		if (list_add_elem(value) == NULL)
+		IntNode *theNode = NodeAtIndex(aList, i);
+		if (NULL != theNode)
 		{
-			break;
+			printf("node[%d]= %d\n", i, theNode->value);
+			 
 		}
 	}
+}
+void Sorting(const List *aList, int Array[5] )
+{
+	int nodeValue;
+	int noArray[5];
+	for (int i = 0; i < CountList(aList); i++)
+	{
+		IntNode *theNode = NodeAtIndex(aList, i);
+		nodeValue = theNode->value;
+		noArray[i] = nodeValue;
+	}
+	IntNode *minNode = findMinNode(aList);
+	int min = minNode->value;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4 - i; j++)
+		{
+			if (noArray[j] > noArray[j + 1])
+			{
+				int tmp = noArray[j];
+				noArray[j] = noArray[j + 1];
+				noArray[j + 1] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		Array[i] = noArray[i];
+		printf("Node[%d] = %d\n", i, Array[i]);
+	}
+
 }
