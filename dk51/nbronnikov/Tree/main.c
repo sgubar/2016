c8ed54c7fa6c6f4e17c19db998dc5f41622fc48e
nbronnikov <nazar_9898@mail.ru>
2016-06-27 00:47:55 +0300

    Create main.c

---
+++ dk51/nbronnikov/Tree/main.c
@@ -0,0 +1,46 @@
+//
+//  Main.c
+//
+//
+//  Created by Nazar Bronnikov on 6/08/16.
+//  Copyright © 2016 Nazar Bronnikov. All rights reserved.
+//
+
+#include <stdio.h>
+#include "tree.h"
+
+int main(int argc, const char * argv[])
+
+#include <stdio.h>
+#include "tree.h"
+
+int main(int argc, const char * argv[])
+{
+	TreePtr theTree = createTree();
+
+	insertNode(theTree, "aaa", "1234");
+	insertNode(theTree, "abb", "1235");
+	insertNode(theTree, "acc", "1232");
+	insertNode(theTree, "baa", "1233");
+
+	displayTree(theTree);
+
+	printf("\ncount before: %d\n\n\n", countTree(theTree->root));
+
+	if(deleteNode(theTree, "baa")==1){
+    printf("\n\n Node (baa) has been deleted\n\n");
+	}else{
+	printf("Node not found\n\n");
+	}
+
+	displayTree(theTree);
+
+
+
+	printf("count after: %d\n\n\n", countTree(theTree->root));
+
+    deleteTree(theTree, theTree->root);
+
+	system("pause");
+	return 0;
+}
