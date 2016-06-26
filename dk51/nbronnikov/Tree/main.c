576b6e9ecbd34c8c2dcf773d8c1df153aefd0c21
nbronnikov <nazar_9898@mail.ru>
2016-06-27 00:52:40 +0300

    Update main.c

--- dk51/nbronnikov/Tree/main.c
+++ dk51/nbronnikov/Tree/main.c
@@ -5,42 +5,29 @@
 //  Created by Nazar Bronnikov on 6/08/16.
 //  Copyright © 2016 Nazar Bronnikov. All rights reserved.
 //
-
-#include <stdio.h>
-#include "tree.h"
-
-int main(int argc, const char * argv[])
-
 #include <stdio.h>
 #include "tree.h"
 
 int main(int argc, const char * argv[])
 {
 	TreePtr theTree = createTree();
-
+	
 	insertNode(theTree, "aaa", "1234");
 	insertNode(theTree, "abb", "1235");
 	insertNode(theTree, "acc", "1232");
 	insertNode(theTree, "baa", "1233");
-
-	displayTree(theTree);
-
-	printf("\ncount before: %d\n\n\n", countTree(theTree->root));
-
-	if(deleteNode(theTree, "baa")==1){
-    printf("\n\n Node (baa) has been deleted\n\n");
-	}else{
-	printf("Node not found\n\n");
+	
+	NodePtr theFindNode = findNode(theTree, "acc");
+	
+	if (deleteNode(theTree, "acc"))
+	{
+		theFindNode = findNode(theTree, "acc");
+		printf ("find result: %x", (unsigned int)theFindNode);
 	}
-
 	displayTree(theTree);
-
-
-
-	printf("count after: %d\n\n\n", countTree(theTree->root));
-
-    deleteTree(theTree, theTree->root);
-
-	system("pause");
+	int counttree = countTree(theTree);
+	printf("counttree = [%d]", counttree);
+	deleteTree(theTree);
+	printf("Hello, World!\n");
 	return 0;
 }
