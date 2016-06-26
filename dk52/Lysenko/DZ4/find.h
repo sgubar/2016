3ff115f60b95ccce5cefaaed596b3383f40ecd81
Anlysenko <drendy2@gmail.com>
2016-06-26 21:01:01 +0300

    Create find.h

---
+++ dk52/Lysenko/DZ4/find.h
@@ -0,0 +1,11 @@
+#ifndef find_h
+#define find_h
+
+#include <stdio.h>
+
+extern const unsigned kNotFound;
+
+unsigned line_find(int anArray[], int aSize, int aKey);
+void UnsortPrint(int anArray[], int aSize);
+
+#endif /* find_h */
