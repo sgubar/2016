db731ee343aad6e5a6642d19bd069e75173e608a
Artem-gaus <gausin.artem@gmail.com>
2016-06-27 01:26:12 +0300

    Update find.c

--- dk52/Gaisin/HW4_find/find.c
+++ dk52/Gaisin/HW4_find/find.c
@@ -3,15 +3,15 @@
 
 const unsigned kNotFound = INT_MAX;
 
-unsigned line_find(int anArray[], int aSize, int aKey)
+unsigned line_find(int anArray[], int aSize, int aKey) //function returned unsigned namber
 {
 	unsigned theResult = kNotFound;
 	
-	for (unsigned i = 0; i < aSize; i++)
+	for (unsigned i = 0; i < aSize; i++) //that would work cycle aSize fold
 	{
-		if (anArray[i] == aKey)
+		if (anArray[i] == aKey) // if value equally aKey, then carry
 		{
-			theResult = i;
+			theResult = i; //assign theResult namber
 			break;
 		}
 	}
