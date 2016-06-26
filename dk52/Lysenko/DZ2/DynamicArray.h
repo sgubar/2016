f6ecc30d441ad61fd1474e0f9812fd65a3c1f0cc
Anlysenko <drendy2@gmail.com>
2016-06-26 20:57:06 +0300

    Create DynamicArray.h

---
+++ dk52/Lysenko/DZ2/DynamicArray.h
@@ -0,0 +1,19 @@
+#ifndef DynamicArray_h
+#define DynamicArray_h
+
+#include <stdio.h>
+
+typedef struct __dynamicIntArray
+{
+	int *storage;
+	int physicalSize;
+	int logicalSize;
+}DynamicIntArray;
+
+DynamicIntArray *createDAInt(int aSize);
+void freeDAInt(DynamicIntArray *anArray);
+
+int valueAtIndex(DynamicIntArray *anArray, int anIndex);
+void setValueAtIndex(DynamicIntArray *anArray, int anValue, int anIndex);
+
+#endif /* DynamicArray_h */
