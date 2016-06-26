fbe1819e07582aa011b8e759f0ac6bcd2e613ef3
mmigush <maxic9898@gmail.com>
2016-06-26 23:40:10 +0300

    Update NewNode.c

--- dk52/Migush/HW_01/NewNode.c
+++ dk52/Migush/HW_01/NewNode.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "NewNode.h"
+
+ Node *CreateNode(int value){
+	Node *tmp=(Node *) malloc(sizeof(Node));
+	tmp->number = value;
+	tmp->next=NULL;
+	return tmp;
+};
+
 
