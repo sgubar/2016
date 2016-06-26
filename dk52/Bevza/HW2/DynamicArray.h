9dbdeab49b7afe72e14ce7508226bc6329fa9784
IrBevza <bevira@ukr.net>
2016-06-27 00:58:30 +0300

    Add files via upload

---
+++ dk52/Bevza/HW2/DynamicArray.h
@@ -0,0 +1,28 @@
+//
+//  DynamicArray.h
+//  doTest
+//
+//  Created by Slava Gubar on 3/24/16.
+//  Copyright © 2016 Slava Gubar. All rights reserved.
+//
+
+#ifndef DynamicArray_h
+#define DynamicArray_h
+
+#include <stdio.h>
+
+typedef struct __dynamicIntArray
+{
+	int *storage;
+	int physicalSize;
+	int logicalSize;
+}DynamicIntArray;
+
+DynamicIntArray *anArray;
+DynamicIntArray *createDAInt(int aSize);
+void freeDAInt(DynamicIntArray *anArray);
+
+int valueAtIndex(DynamicIntArray *anArray, int anIndex);
+void setValueAtIndex(DynamicIntArray *anArray, int anValue, int anIndex);
+
+#endif /* DynamicArray_h */
