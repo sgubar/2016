7998a78d9227fc16525d75536d6d48a39836ae21
mmigush <maxic9898@gmail.com>
2016-06-27 08:12:40 +0300

    Create filling.h

---
+++ dk52/Migush/Lab_3/filling.h
@@ -0,0 +1,8 @@
+#ifndef quickSort_h
+#define quickSort_h
+
+int gener_array(int anArray[], int anArray[],int anArray[],int anArray[],int anArray[]);
+void printArray(int anArray[], int aSize);
+
+#endif
+
